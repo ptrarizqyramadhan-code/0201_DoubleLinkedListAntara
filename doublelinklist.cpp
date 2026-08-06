6480aecdd6832622090169ab57bb55d9199db795
ptrarizqyramadhan-code <ptrarizqyramadhan@gmail.com>
2026-08-06 10:41:21 +0700

    Insert In between/end

--- doublelinklist.cpp
+++ doublelinklist.cpp
@@ -56,3 +56,17 @@ public:
             START = newNode;
             return;
         }
+
+        // Insert in between/end
+        // Step 8: Locate position for insertion
+        Node *current = START;
+        while (current->next != NULL && current->next->noMhs < nim)
+        {
+            current = current->next;
+        }
+
+        if (current->next != NULL && nim == current->next->noMhs)
+        {
+            cout << "\nDuplicate roll numbers not allowed" << endl;
+            return;
+        }
